4d0d9f987801bc97b3f538b74a9173c0d3b761ca
Mayank1227 <patelmayank1351@gmail.com>
2024-01-04 11:41:49 +0530

    4/1/2024

---
+++ 5arraymethads.js
@@ -0,0 +1,22 @@
+ //....array methads........
+        //  push () : add to end
+        //  pop():delet form end & eturn
+        // tostring() converts array to string
+        // 
+         //  push () : add to end
+        //   let foodItems = ["apple", "potato","litchi"];
+        //   foodItems.push ("chips","burger");
+        //   console.log( foodItems)
+        //   
+         //  pop():delet form end & eturn
+        //  let foodItems = ["apple", "potato","litchi"];
+        //   console.log( foodItems);
+        //   let deletedItem =foodItems.pop();
+        //   console.log(foodItems);
+        //   console.log("deleted",deletedItem);
+        // 
+        // tostring() converts array to string
+        // let foodItems = ["apple", "potato","litchi"];
+        //   console.log( foodItems);
+        //   let deletedItem =foodItems.toString();
+        //   console.log(foodItems);
