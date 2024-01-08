4b29f17316d9c1899146217cc7b8951b06d61872
Mayank1227 <patelmayank1351@gmail.com>
2024-01-08 11:32:21 +0530

    8/1/2024

---
+++ 9arrowfunction.js
@@ -0,0 +1,12 @@
+    // const printHello =()=>{
+        //    consolo.log("hello");
+        // };
+        
+        //  
+        // let arrowmul = (a ,b) =>{
+        //      return a*b;
+        // };
+        // // 
+        // let arrowsum = (a ,b) =>{
+        //      return a+b;
+        // };
